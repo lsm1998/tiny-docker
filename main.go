2ed1a1d4f9631a4c265e92380e5e7264ad311f38
lsm1998 <487005831@qq.com>
2026-05-09 18:17:55 +0000

    无用代码删除

--- main.go
+++ main.go
@@ -1,32 +1,10 @@
 package main
 
 import (
-	"errors"
-	"io/fs"
 	"os"
 	"tinydocker/cli"
 	"tinydocker/pkg/system"
 )
-
-const (
-	def = "/var/lib/tinydocker/"
-)
-
-func init() {
-	info, err := os.Stat(def)
-	if errors.Is(err, os.ErrNotExist) {
-		if err = os.MkdirAll(def, fs.ModePerm); err != nil {
-			system.Panic("创建容器目录失败，请检查权限，err=%s", err.Error())
-		}
-		return
-	}
-	if err != nil {
-		system.Panic("检查容器目录失败，err=%s", err.Error())
-	}
-	if !info.IsDir() {
-		system.Panic("存在和容器目录同名的文件，请先清理")
-	}
-}
 
 func main() {
 	if len(os.Args) <= 1 {
